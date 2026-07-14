0fc57fa2389846f0c7dfe40f87665c94db547036
Prakhar Maitreya Parashar <prakharw23@gmail.com>
2026-07-14 11:19:23 +0530

    Portfolio all 4 routes

--- src/main/java/com/prakhar/demo/Portfolio/MyPortfolio.java
+++ src/main/java/com/prakhar/demo/Portfolio/MyPortfolio.java
@@ -19,4 +19,46 @@ public class MyPortfolio
                 </ul>
                 """;
     }
+
+    @GetMapping("/education")
+    public String Education()
+    {
+        return """
+                <h1>Education</h1>
+                <p></p>
+                <ul>
+                <li>Lovely Professional University: Current CGPA: 7.56</l1>
+                <li>12th: 68.6%</li>
+                <li>10th: 86.8%</li>
+                </ul>
+                """;
+    }
+
+    @GetMapping("/skills")
+    public String Skills()
+    {
+        return """
+                <h1>Skills</h1>
+                <p>The following are my technical skills</p>
+                <ul>
+                <li>Programming Languages: C++, c, Java, Python, Javascript</l1>
+                <li>Frameworks: Spring, SpringBoot, Django, React, Node</li>
+                <li>Tools: MySQL, PostGresSQL, NoSQL, GIT</li>
+                </ul>
+                """;
+    }
+
+    @GetMapping("/project")
+    public String Project()
+    {
+        return """
+                <h1>Projects</h1>
+                <p>The following are my projects</p>
+                <ul>
+                <li><a href="https://github.com/Prakhar-Maitreya/Library-Management-System">Library Management System Backend API</a></li>
+                <li><a href="https://github.com/Prakhar-Maitreya/Employee-Portal-Cipherschools">Employee Management System</a></li>
+                <li><a href="https://github.com/Prakhar-Maitreya/Rainwater-Harvesting-Mini-website">Rainwater Harvesting Awareness website</a></li>
+                </ul>
+                """;
+    }
 }

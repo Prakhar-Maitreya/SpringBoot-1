558e122ac70ecc90d2cb3586730eda5287c9612e
Prakhar Maitreya Parashar <prakharw23@gmail.com>
2026-07-14 12:56:43 +0530

    POST with student

---
+++ src/main/java/com/prakhar/demo/StudentServer/Student.java
@@ -0,0 +1,41 @@
+package com.prakhar.demo.StudentServer;
+
+public class Student
+{
+    int id;
+    String name;
+    int age;
+    String department;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+}

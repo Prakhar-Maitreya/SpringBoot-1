dc20d8d8ea3afd6e211d4eae074fb542343fbabb
Prakhar Maitreya Parashar <prakharw23@gmail.com>
2026-07-18 11:28:48 +0530

    Saturday

--- src/main/java/com/prakhar/demo/DependencyInjection/OrderService.java
+++ src/main/java/com/prakhar/demo/DependencyInjection/OrderService.java
@@ -1,5 +1,6 @@
 package com.prakhar.demo.DependencyInjection;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
---
+++ src/main/java/com/prakhar/demo/StudentServer/DTO/ResponseStudentDTO.java
@@ -0,0 +1,5 @@
+package com.prakhar.demo.StudentServer.DTO;
+
+public class ResponseStudentDTO {
+
+}
